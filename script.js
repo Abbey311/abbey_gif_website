b9a7c29ef4ff442d37f3c966ebbe9b44e86dd855
Abbey311 <y.abbey2@gmail.com>
2021-06-09 18:52:21 -0400

    gif limit on page

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ console.log("page loaded");
 
 const gifForm = document.querySelector("form");
 const gifResults = document.querySelector("#gif-results");
+const loadButton = document.querySelector(".hidden");
 const apiKey = "ZzGHO6UA4jAo0fU6hInmEDLLEgejNi6k";
-// const limit;
-// const rating;
+const limit = 9;
+const rating = "g";
+let pages = 0;
+let offset = pages*limit;
+
 gifForm.addEventListener("submit", handleFormSubmit);
+loadButton.addEventListener("click", showMore);
+
 
 async function getResults(search_term) {
-    const apiUrl = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + search_term;
-    //console.log(apiUrl);
+    const apiUrl = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + search_term + "&limit=" + limit + "&offset=" + offset;
 
     // 2. On form submit, go to the gif API 
     const response = await fetch(apiUrl);
@@ -33,4 +38,9 @@ async function getResults(search_term) {
       const gif = gifInput.value;
       gifResults.innerHTML =` `;
       getResults(gif);
+  }
+
+  function showMore(event){
+    getResults();
+    pages+=1;
   }
