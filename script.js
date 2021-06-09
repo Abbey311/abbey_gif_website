ee9578c23714c3633c309c358a735f872e58820c
Abbey311 <y.abbey2@gmail.com>
2021-06-09 17:09:29 -0400

    page loads gifs

--- script.js
+++ script.js
@@ -2,4 +2,30 @@ console.log("page loaded");
 
 const gifForm = document.querySelector("form");
 const gifResults = document.querySelector("#gif-results");
+const apiKey = "ZzGHO6UA4jAo0fU6hInmEDLLEgejNi6k";
+// const limit;
+// const rating;
+gifForm.addEventListener("submit", getResults);
 
+async function getResults(event) {
+    event.preventDefault();
+    const gifInput = event.target.Gif;
+    const gif = gifInput.value;
+    const apiUrl = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + gif;
+    //console.log(apiUrl);
+
+    // 2. On form submit, go to the gif API 
+    const response = await fetch(apiUrl);
+    const responseData = await response.json();
+    const data = responseData.data
+  
+   data.forEach(element => displayResults(element));
+   return data;
+
+  }
+
+  function displayResults(element) {
+      gifResults.innerHTML+=`
+      <img src="${element.images.original.url} "alt=${element.title}/>"
+      `
+  } 
